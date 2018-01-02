6f166781907059bdc4bc2cd893791cd08e91440d
Aevit <Aevitx@gmail.com>
2018-01-02 15:54:09 +0800

    add ios static library project

---
+++ ios/RNKitMoXie.h
@@ -0,0 +1,14 @@
+//
+//  RNKitMoXie.h
+//  RNKitMoXie
+//
+//  Created by aevit on 2018/1/2.
+//  Copyright © 2018年 aevit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface RNKitMoXie : NSObject <RCTBridgeModule>
+
+@end
---
+++ ios/RNKitMoXie.m
@@ -0,0 +1,23 @@
+//
+//  RNKitMoXie.m
+//  RNKitMoXie
+//
+//  Created by aevit on 2018/1/2.
+//  Copyright © 2018年 aevit. All rights reserved.
+//
+
+#import "RNKitMoXie.h"
+
+@implementation RNKitMoXie
+
+RCT_EXPORT_MODULE(RNKitMoXie)
+
+RCT_EXPORT_METHOD(initial:(NSString*)userId apiKey:(NSString*)apiKey) {
+    
+}
+
+RCT_EXPORT_METHOD(start:(NSString*)functionName) {
+    
+}
+
+@end
